6a3855f718fc88b04f4152aabcccbe75643d893b
MesfinGithinji <meshgithinji@gmail.com>
2023-08-24 02:43:44 +0300

    Fix cicular imports

--- models/state.py
+++ models/state.py
@@ -7,29 +7,26 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel, Base):
     """State class defined attributes and columns """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref='state',
+                          cascade='all, delete, delete-orphan')
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128),nullable=False)
-        cities = relationship("City", cascade="all, delete",backref="states")
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """sets up state"""
-        super().__init__(*args, **kwargs)
-
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-        @property
-        def cities(self):
-            """method to return city instances"""
-            city_details = models.storage.all("City").values()
-            city_val = []
-            for detail in city_details:
-                if detail.state_id == self.id:
-                    city_val.append(detail)
-            return city_val
+    @property
+    def cities(self):
+        """
+        returns a list of City instances where state_id equals
+        to current State.id
+        """
+        from models import storage
+        city_states = []
+        cities = storage.all(City)
+        for city in cities.values():
+            if city.state_id == self.id:
+                city_states.append(city)
+        return city_states

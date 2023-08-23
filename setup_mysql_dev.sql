b682631bdedb6d232d622406ad07a83f44d50081
MesfinGithinji <meshgithinji@gmail.com>
2023-08-23 16:23:17 +0300

    SQL script to set up the database,create user and grant priviledges

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- sets up a MySQL server for the project
+-- creates the database, a new user and grant privileges
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev' @'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev' @'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev' @'localhost';

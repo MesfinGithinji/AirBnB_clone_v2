7a85593b3a92e814ef64fe6083a0075883d18413
MesfinGithinji <meshgithinji@gmail.com>
2023-08-24 04:41:42 +0300

    Revert "SQL update"
    This reverts commit 9df52571958ebf9138f8a44e5024a8e005b784a5.

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,5 +1,4 @@
--- Creates a database  for the project
--- creates a new user and grant privileges
+-- tests for database setup
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test' @'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test' @'localhost';

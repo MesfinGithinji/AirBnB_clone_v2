919684744feef0c716861e608ee7fae7745a9b91
MesfinGithinji <meshgithinji@gmail.com>
2023-08-24 04:40:45 +0300

    Revert "Fix cicular imports"
    This reverts commit 6a3855f718fc88b04f4152aabcccbe75643d893b.

--- models/review.py
+++ models/review.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, ForeignKey
+
 import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 
 class Review(BaseModel, Base):
     """Review class attributed and table columns"""
-    __tablename__ = 'reviews'
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    text = Column(String(1024), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'reviews'
+        text = Column(String(1024),nullable=False)
+        place_id = Column(String(60),ForeignKey('places.id'),nullable=False)
+        user_id = Column(String(60),ForeignKey('users.id'),nullable=False)
+    else:
+        text = ""
+        place_id = ""
+        user_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """Review initialized"""
+        super().__init__(*args, **kwargs)

90ef37dcded9dbb31be86ec7a2bac2d90dbd3bd1
MesfinGithinji <meshgithinji@gmail.com>
2023-09-23 04:13:53 +0300

    Added the /c route and defined <text> variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application.
+"""
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns a string"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns a string"""
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ctext(text):
+    """display C followed by the value of the text variable.
+        replace underscore _ symbols with a space
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)

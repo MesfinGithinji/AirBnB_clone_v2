edb2c97d747aaf54e9f6f70094acdc8e688f99fa
MesfinGithinji <meshgithinji@gmail.com>
2023-09-25 01:34:37 +0300

    Fix

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -5,7 +5,7 @@ A script that starts a Flask web application.
 
 from flask import Flask, render_template, abort
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
